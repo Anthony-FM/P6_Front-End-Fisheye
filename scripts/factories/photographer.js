40c14cdadcfb6ce81f089e763ac63b1b699e16bf
Anthony Fouda Many <anthony.foudamany@gmail.com>
2022-09-16 11:13:36 +0200

    :hammer: Ajout des données JSON page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,41 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+ function photographerFactory(data) {
+    const { name,city,country,tagline,price,portrait } = data;
+    // console.log({name,city,country,portrait, tagline, price});
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/Photographers_ID_Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const div1 = document.createElement( 'div');
+        const div2 = document.createElement( 'div');
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        div1.appendChild(img);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        div1.appendChild(h2);
+
+        const pCountry = document.createElement( 'p' );
+        pCountry.className = "photographer_country";
+        pCountry.textContent = city + ", " + country;
+        div2.appendChild(pCountry);
+
+        const taglines = document.createElement( 'p' );
+        taglines.className = "photographer_tagline";
+        taglines.textContent = tagline;
+        div2.appendChild(taglines);
+
+        const prices = document.createElement( 'p' );
+        prices.className = "photographer_price";
+        prices.textContent = price + "€/jour";
+        div2.appendChild(prices);
+
+        article.appendChild(div1);
+        article.appendChild(div2);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    
+    return { name, picture, getUserCardDOM}
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,12 @@
+    // import { photographerFactory } from "./factories/photographer.js";
+
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const photographerJSON = await fetch ("./data/photographers.json");
+        const photographers = await photographerJSON.json();
+        console.log(photographers)
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return photographers;
     }
 
     async function displayData(photographers) {
@@ -30,7 +14,9 @@
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
+            // console.log(photographerModel);
             const userCardDOM = photographerModel.getUserCardDOM();
+            // console.log(userCardDOM);
             photographersSection.appendChild(userCardDOM);
         });
     };
