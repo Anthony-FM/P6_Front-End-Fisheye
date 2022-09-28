ee685e6604ac6ff24a79fdde624badc3b0f76386
Anthony Fouda Many <anthony.foudamany@gmail.com>
2022-09-28 12:20:28 +0200

    :sparkles: Ajout 2 fonctions (getUserName, getUserPicture) pour photographe.js :sparkles:

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -13,9 +13,9 @@
         const sectionImageArticleLink = document.createElement( 'a' ); // Création des liens
         sectionImageArticleLink.appendChild(sectionImageArticle);
         sectionImageArticleLink.setAttribute("id", id);
-        sectionImageArticleLink.setAttribute("href", `photographer.html?${id}/${name}`);
+        sectionImageArticleLink.setAttribute("href", `photographer.html?id=${id}&${name}`);
 
-        const footerImageArticle = document.createElement( 'footer' ); // création ddu footer pour le contenu
+        const footerImageArticle = document.createElement( 'footer' ); // création du footer pour le contenu
 
         const img = document.createElement( 'img' ); // création de la balise photo
         img.setAttribute("src", picture);
@@ -46,7 +46,42 @@
         return (article);
     }
 
-    return {getUserCardDOM}
+    function getUserNameDOM() { // Fonction qui récupère uniquement les name, city/country et tagline
+
+        const userTagName = document.createElement( 'article' );
+
+        const userName = document.createElement( 'h1' ); // Création d'un h1 pour le prénom
+        userName.className = "photographerPage_name";
+        userName.textContent = name;
+
+        const userCountry = document.createElement( 'p' ); // Création d'un p pour le pays et la ville du photographe
+        userCountry.className = "photographerPage_country";
+        userCountry.textContent = city + ", " + country;
+
+        const userTagline = document.createElement( 'p' );
+        userTagline.className = "photographerPage_tagline";
+        userTagline.textContent = tagline;
+
+        userTagName.appendChild(userName)
+        userTagName.appendChild(userCountry)
+        userTagName.appendChild(userTagline)
+
+        return userTagName;
+    }
+
+    function getUserPictureDOM() { // fonction qui récupère uniquement la photo de profil du photographe
+        
+        const imgUser = document.createElement( 'img' ); // création de la balise photo
+        imgUser.setAttribute("src", picture);
+        imgUser.setAttribute("alt", "Profil de " + name);
+
+        const userPicture = document.createElement( 'article' );
+        userPicture.appendChild(imgUser)
+
+        return userPicture;
+    }
+
+    return {name, id, city, country, tagline, price, portrait, getUserCardDOM, getUserNameDOM, getUserPictureDOM}
 }
 
 
