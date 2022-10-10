5f95b7cfa06344a66c57834b90eb54b19688e855
Anthony Fouda Many <anthony.foudamany@gmail.com>
2022-10-10 16:26:47 +0200

    :zap: Ajout accessibilité

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,38 +6,47 @@
 
     function getUserCardDOM() { // Création des articles du DOM pour chaque photographeurs
         const article = document.createElement( 'article' ); // Création de la balise article
+        article.setAttribute("tabindex", "2");
+        article.setAttribute("aria-label", `Selectionné du profil du photographe ${name}`)
 
         const sectionImageArticle = document.createElement( 'section' ); // création de la section cliquable
         sectionImageArticle.className = "photographer_pictureName";
+        
 
         const sectionImageArticleLink = document.createElement( 'a' ); // Création des liens
         sectionImageArticleLink.appendChild(sectionImageArticle);
         sectionImageArticleLink.setAttribute("id", id);
         sectionImageArticleLink.setAttribute("href", `photographer.html?id=${id}&${name}`);
+        
 
         const footerImageArticle = document.createElement( 'footer' ); // création du footer pour le contenu
 
         const img = document.createElement( 'img' ); // création de la balise photo
         img.setAttribute("src", picture);
-        img.setAttribute("alt", "Profil de " + name);
+        img.setAttribute("alt", "Profil de " + name );
+        img.setAttribute("tabindex", "2");  
         sectionImageArticle.appendChild(img); // ajout de l'image dans la div image
 
         const h2 = document.createElement( 'h2' ); // création du titre h2 pour les noms des photographes
         h2.textContent = name;
+        h2.setAttribute("tabindex", "2");  
         sectionImageArticle.appendChild(h2);
 
         const pCountry = document.createElement( 'h3' ); // Création des titre h3 pour les villes et pays de chaque photographe
         pCountry.className = "photographer_country";
+        pCountry.setAttribute("tabindex", "2");  
         pCountry.textContent = city + ", " + country;
         footerImageArticle.appendChild(pCountry);
 
         const taglines = document.createElement( 'p' ); // Création d'un paragraphe "p" pour leur slogan
         taglines.className = "photographer_tagline";
+        taglines.setAttribute("tabindex", "2");  
         taglines.textContent = tagline;
         footerImageArticle.appendChild(taglines);
 
         const prices = document.createElement( 'p' ); // Création d'un paragraphe pour leur prix journalier
         prices.className = "photographer_price";
+        prices.setAttribute("tabindex", "2  ");  
         prices.textContent = price + "€/jour";
         footerImageArticle.appendChild(prices);
 
@@ -53,14 +62,17 @@
         const userName = document.createElement( 'h1' ); // Création d'un h1 pour le prénom
         userName.className = "photographerPage_name";
         userName.textContent = name;
+        userName.setAttribute("tabindex","2");
 
         const userCountry = document.createElement( 'p' ); // Création d'un p pour le pays et la ville du photographe
         userCountry.className = "photographerPage_country";
         userCountry.textContent = city + ", " + country;
+        userCountry.setAttribute("tabindex","2");
 
         const userTagline = document.createElement( 'p' );
         userTagline.className = "photographerPage_tagline";
         userTagline.textContent = tagline;
+        userTagline.setAttribute("tabindex","2");
 
         userTagName.appendChild(userName)
         userTagName.appendChild(userCountry)
@@ -74,6 +86,7 @@
         const imgUser = document.createElement( 'img' ); // création de la balise photo
         imgUser.setAttribute("src", picture);
         imgUser.setAttribute("alt", "Profil de " + name);
+        imgUser.setAttribute("tabindex","2");
 
         const userPicture = document.createElement( 'article' );
         userPicture.appendChild(imgUser)
@@ -99,6 +112,7 @@ function mediaFactory(data){
         let imgPictures = document.createElement( 'img' );
         imgPictures.setAttribute("src", urlName + image);
         imgPictures.setAttribute("alt", "Picture of " + title );
+        imgPictures.setAttribute("tabindex","6");
 
         let videoPictures = document.createElement( 'video' );
         videoPictures.setAttribute("controls","");
@@ -107,6 +121,7 @@ function mediaFactory(data){
         sourceVideo.setAttribute("alt", "Video of" + title );
         sourceVideo.setAttribute("type", "video/mp4");
         videoPictures.appendChild(sourceVideo);
+        videoPictures.setAttribute("tabindex","6");
 
         if (image) {
             header.appendChild(imgPictures);
@@ -121,6 +136,7 @@ function mediaFactory(data){
         let titlePictures = document.createElement( 'h2' );
         titlePictures.className = "title-pictures";
         titlePictures.textContent = title;
+        titlePictures.setAttribute("tabindex","6");
         footerPicture.appendChild(titlePictures)
 
         let heartNumberContainer = document.createElement( 'div' );
@@ -129,21 +145,26 @@ function mediaFactory(data){
         let heartNumber = document.createElement( 'p' );
         heartNumber.className = "heart-number";
         heartNumber.textContent = likes;
+        heartNumber.setAttribute("tabindex","6");
 
         let heart = document.createElement( 'em' );
         heart.className = "fa-regular fa-heart";
-        heart.setAttribute('aria-label', 'likes')
+        heart.setAttribute('role','button');                            
+        heart.setAttribute("tabindex","6");
         heart.addEventListener('click', () => {
             if(heart.classList.contains = "fa-regular"){
                 heart.classList.replace("fa-regular", "fa-solid");
+                heart.classList.add("heart");
             } else {
                 heart.classList.replace("fa-solid", "fa-regular");
+                heart.classList.remove("heart");
             }
                 
         })
 
         heart.setAttribute('aria-hidden','false');
-        heart.setAttribute("title", "Nombre de like pour cette photo. cliquez pour ajouter un like")
+        heart.setAttribute("title", "likes");
+        heart.setAttribute('aria-label', `La photo a ${likes} likes . Cliquez pour ajouter ou retirer un like`);
 
         heartNumberContainer.appendChild(heartNumber);
         heartNumberContainer.appendChild(heart);
@@ -159,6 +180,8 @@ function mediaFactory(data){
     function getLikesNumbers() {
         let countContainer = document.createElement( 'aside');
         countContainer.className = "count-container";
+        countContainer.setAttribute('aria-label', 'Nombre de likes total et Prix journalier du photographe');
+        countContainer.setAttribute('tabindex', `7`);
 
         let totalHeartNumber = document.createElement( 'p' );
         totalHeartNumber.className = "hearts-number";
@@ -170,18 +193,22 @@ function mediaFactory(data){
         }
         
         totalHeartNumber.innerHTML = `${totals} <em class="fa-solid fa-heart" aria-hidden="true" title="Nombre de like total de toutes les photos"></em><br/>
-        <span class="screenreader-text">Nombre de like pour cette photo</span>`;
+        <span class="screenreader-text">Nombre de like pour ce photographe  </span>`;
+        totalHeartNumber.setAttribute('tabindex', '7');
 
         
 
         let priceByDay = document.createElement( 'p' );
-        priceByDay.textContent = `${price}€ / jour`;
+        priceByDay.setAttribute('id','priceByDay')
+        priceByDay.innerHTML = `${price}€ / jour <br/>
+        <span class="screenreader-text" aria-labelled="priceByDay">Prix par jour du photographe </span>`;
+        priceByDay.setAttribute('tabindex', '7');
 
         countContainer.appendChild(totalHeartNumber);
         countContainer.appendChild(priceByDay);
 
         return countContainer;
-    }
+    }   
 
     return {id, photographerId, title, image, likes, date, price,getUserMediasDOM, getLikesNumbers}
 }
